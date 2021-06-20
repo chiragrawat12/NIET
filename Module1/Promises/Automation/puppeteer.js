ed4e33b60df5a74665c89ceca9ff795dd78b7a3b
chiragrawat12 <chiragrawat12@gmail.com>
2021-06-20 15:28:10 +0530

    Completed hackerrank bot using promise

--- Module1/Promises/Automation/puppeteer.js
+++ Module1/Promises/Automation/puppeteer.js
@@ -1,9 +1,11 @@
 let puppeteer = require("puppeteer");
+let solution = require("./solution");
 
 const id = `ronefag160@moxkid.com`;
 const password = "123456789";
 
 let tab;
+let browser;
 
 let browserOpenPriomise = puppeteer.launch({
   headless: false,
@@ -14,6 +16,7 @@ let browserOpenPriomise = puppeteer.launch({
 
 browserOpenPriomise
   .then(function (browserInstance) {
+    browser = browserInstance;
     let pagesPromise = browserInstance.pages();
     return pagesPromise;
   })
@@ -40,43 +43,157 @@ browserOpenPriomise
     return loginButtonPromise;
   })
   .then(function () {
-    let waitPromise = tab.waitForSelector("#base-card-1-link");
+    let waitAndClickPromise = waitAndClick("#base-card-1-link" , tab);
+    return waitAndClick;
+  })
+  .then(function () {
+    let waitAndClickPromise = waitAndClick("a[data-attr1='warmup']" , tab);
+    return waitAndClickPromise;
+  })
+  .then(function () {
+    let waitPromise = tab.waitForSelector(
+      ".js-track-click.challenge-list-item"
+    );
     return waitPromise;
   })
   .then(function () {
-    let interviewPreparationPromise = tab.click("#base-card-1-link");
+    let allChallengesTags = tab.$$(".js-track-click.challenge-list-item");
+    return allChallengesTags;
+  })
+  .then(function (allChallengesTags) {
+    let allQuesLinkPromise = [];
+    for (let i = 0; i < allChallengesTags.length; i++) {
+      let quesLinkPromise = tab.evaluate(function (ele) {
+        return ele.getAttribute("href");
+      }, allChallengesTags[i]);
+      allQuesLinkPromise.push(quesLinkPromise);
+    }
 
-    return interviewPreparationPromise;
+    let combinedPromise = Promise.all(allQuesLinkPromise);
+    return combinedPromise;
+  })
+  .then(function (combinedLinks) {
+    let oneQuesLinkPromise = solveQuestion(combinedLinks[0]);
+    
+    for(let i = 1 ; i < combinedLinks.length ; i++){
+
+      oneQuesLinkPromise = oneQuesLinkPromise.then(function(){
+        let nextQuesSolvePromise = solveQuestion(combinedLinks[i]);
+        return nextQuesSolvePromise;
+
+      });
+
+    }
+
+    return oneQuesLinkPromise;
   })
   .then(function () {
-    let waitPromise = tab.waitForSelector("a[data-attr1='warmup']");
-    return waitPromise;
+    console.log("All Questions Solved !!");
   })
-  .then(function(){
-      let warupPromise = tab.click("a[data-attr1='warmup']");
-      return warupPromise;
-  })
-  .then(function(){
-    let waitPromise = tab.waitForSelector(".js-track-click.challenge-list-item");
-    return waitPromise;
-  })
-  .then(function(){
-      let allChallengesTags = tab.$$(".js-track-click.challenge-list-item");
-      return allChallengesTags;
-  })
-  .then(function(allChallengesTags){
-      let allQuesLinkPromise = [];
-      for(let i = 0 ; i < allChallengesTags.length ; i ++){
-        let quesLinkPromise = tab.evaluate(function(ele){ return ele.getAttribute("href"); } , allChallengesTags[i]);
-        allQuesLinkPromise.push(quesLinkPromise);
-      }
-
-      let combinedPromise = Promise.all(allQuesLinkPromise);
-      return combinedPromise;
-  })
-  .then(function(combinedLinks){
-    console.log(combinedLinks);
-  })
-  .catch(function (error) {
-    console.log(error);
+  .catch(function (err) {
+    console.log("Inside catch");
+    console.log(err);
   });
+
+function waitAndClick(selector , tab) {
+  return new Promise(function (scb, fcb) {
+    let waitPromise = tab.waitForSelector(selector);
+    waitPromise
+      .then(function () {
+        let btnClickPromise = tab.click(selector);
+        return btnClickPromise;
+      })
+      .then(function () {
+        scb();
+      })
+      .catch(function () {
+        fcb();
+      });
+  });
+}
+
+function solveQuestion(questionLink) {
+  return new Promise(function(scb , fcb){
+    let completeLink = "https://www.hackerrank.com" + questionLink;
+    let tab;
+    let code;
+    let newTabPromise = browser.newPage();
+    newTabPromise
+      .then(function (newTab) {
+        tab = newTab;
+        let gotoQuesPromise = tab.goto(completeLink);
+        return gotoQuesPromise;
+      })
+      .then(function () {
+        let quesNamePromise = tab.$(".ui-icon-label.page-label");
+        return quesNamePromise;
+      })
+      .then(function (quesNameH1Tag) {
+        let quesNamePromise = tab.evaluate(function (ele) {
+          return ele.textContent;
+        }, quesNameH1Tag);
+        return quesNamePromise;
+      })
+      .then(function(quesName){
+        for(let i = 0 ; i < solution.length ; i++){
+          if(solution[i].name == quesName){
+            code = solution[i].sol;
+            break;
+          }
+        }
+        let clickPromise = waitAndClick(".checkbox-input", tab);
+        return clickPromise;
+      })
+      .then(function(){
+        let waitPromise = tab.waitForTimeout(1000);
+        return waitPromise;
+      })
+      .then(function(){
+        let codeTypePromise = tab.type("#input-1", code);
+        return codeTypePromise;
+      })
+      .then(function(){
+        let ctrlKeyDown = tab.keyboard.down("Control");
+        return ctrlKeyDown;
+      })
+      .then(function(){
+        let aKeyPress = tab.keyboard.press('a');
+        return aKeyPress;
+      })
+      .then(function(){
+        let xkeyPress = tab.keyboard.press("x");
+        return xkeyPress;
+      })
+      .then(function(){
+        let codeBoxClickedPromise = tab.click(
+          ".monaco-scrollable-element.editor-scrollable.vs"
+        );
+        return codeBoxClickedPromise;
+      })
+      .then(function () {
+        let aKeyPress = tab.keyboard.press("a");
+        return aKeyPress;
+      })
+      .then(function(){
+        let vKeyPromise = tab.keyboard.press('v');
+        return vKeyPromise;
+      })
+      .then(function(){
+        let clickSubmitPromise = tab.click(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled");
+        return clickSubmitPromise;
+      })
+      .then(function(){
+        let waitSuccessPromise = tab.waitForSelector('.compiler-message__value code');
+        return waitSuccessPromise;
+      })
+      .then(function(){
+        return tab.close();
+      })
+      .then(function(){
+        scb();
+      })
+      .catch(function(error){
+        fcb(error);
+      })
+    });
+}
